341f1d7d60a6ba11b7e2495cadb171017dd17e49
Nick Jaczko <nicholas.jaczko@liveramp.com>
2024-12-12 12:55:03 -0500

    wip

--- lua/nvim-psql.lua
+++ lua/nvim-psql.lua
@@ -6,8 +6,8 @@ function exec(cmd)
   return output
 end
 
-function M.init(dsn)
-  -- if (dsn == nil or dsn == '') then error("Postgres DSN is required") end
+function M.initPSQL(dsn)
+  if (dsn == nil or dsn == '') then error("Postgres DSN is required") end
 
   local nvim_psql_dir = os.getenv('HOME') .. '/.nvim-psql/'
   if vim.fn.isdirectory(nvim_psql_dir) == 0 then
@@ -24,14 +24,44 @@ function M.init(dsn)
   vim.opt.ft='sql'
 end
 
-function M.query()
+function M.initBigQuery()
+  local nvim_psql_dir = os.getenv('HOME') .. '/.nvim-psql/'
+  if vim.fn.isdirectory(nvim_psql_dir) == 0 then
+    vim.fn.mkdir(nvim_psql_dir)
+  end
+
+  -- TODO DRY in initPSQL. Or, make new files for the BQ queries
+  M.query_file_path = nvim_psql_dir .. 'query'
+  M.output_file_path = nvim_psql_dir .. 'output'
+  M.history_file_path = nvim_psql_dir .. 'history'
+  M.postgres_dsn = dsn
+
+  vim.cmd.edit(M.output_file_path)
+  vim.cmd.vsplit(M.query_file_path)
+  vim.opt.ft='sql'
+end
+
+function M.psqlQuery()
   vim.cmd.wa()
   stdout = exec(string.format(
-  -- bq query --max_rows=100000 --nouse_legacy_sql "$(cat bq.sql)"
+     "psql -d '%s' -f '%s' -o '%s'",
+     M.postgres_dsn, M.query_file_path, M.output_file_path
+  ))
+  exec(string.format(
+    "cat %s >> %s",
+    M.query_file_path, M.history_file_path
+  ))
+  vim.cmd.bufdo('e')
+  print(stdout)
+end
+
+function M.bigQuery()
+  vim.cmd.wa()
+  stdout = exec(string.format(
     "bq query --max_rows=100000 --nouse_legacy_sql \"$(cat %s)\" > %s",
     M.query_file_path, M.output_file_path
-    -- M.postgres_dsn, M.query_file_path, M.output_file_path
   ))
+  -- TODO DRY
   exec(string.format(
     "cat %s >> %s",
     M.query_file_path, M.history_file_path
